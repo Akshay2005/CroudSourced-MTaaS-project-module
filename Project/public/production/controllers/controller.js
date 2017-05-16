643b1fb4317a2ed01073777d98e35daedb9a3cbc
Akshay Sonvane <akshay.sonvane93@gmail.com>
2017-05-15 21:23:59 -0700

    Fixed Session Mgmt.

--- Project/public/production/controllers/controller.js
+++ Project/public/production/controllers/controller.js
@@ -9,40 +9,40 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 			templateUrl: 'pages/dashboard.html',
 			controller: 'DashboardController'
 		}).state('addproject', {
-		url: "/addproject",
-		templateUrl: 'pages/addproject.html',
-		controller: 'AddProjectController'
-	}).state('viewprojects', {
-		url: "/viewprojects",
-		templateUrl: 'pages/viewprojects.html',
-		controller: 'ViewProjectsController'
-    }).state('myProject', {
-        url: "/myProject",
-        templateUrl: 'pages/myProject.html',
-        controller: 'myProjectController'
-	}).state('profile', {
-		url: "/profile",
-		templateUrl: 'pages/profile.html',
-		controller: 'ProfileController'
-	}).state('projectmanager', {
-		url: "/manager",
-		templateUrl: 'pages/users.html',
-		controller: 'ViewUsers'
-	}).state('testers', {
-		url: "/tester",
-		templateUrl: 'pages/users.html',
-		controller: 'ViewUsers'
-	}).state('customers', {
-		url: "/customer",
-		templateUrl: 'pages/users.html',
-		controller: 'ViewUsers'
-	})
+			url: "/addproject",
+			templateUrl: 'pages/addproject.html',
+			controller: 'AddProjectController'
+		}).state('viewprojects', {
+			url: "/viewprojects",
+			templateUrl: 'pages/viewprojects.html',
+			controller: 'ViewProjectsController'
+		}).state('myProject', {
+			url: "/myProject",
+			templateUrl: 'pages/myProject.html',
+			controller: 'myProjectController'
+		}).state('profile', {
+			url: "/profile",
+			templateUrl: 'pages/profile.html',
+			controller: 'ProfileController'
+		}).state('projectmanager', {
+			url: "/manager",
+			templateUrl: 'pages/users.html',
+			controller: 'ViewUsers'
+		}).state('testers', {
+			url: "/tester",
+			templateUrl: 'pages/users.html',
+			controller: 'ViewUsers'
+		}).state('customers', {
+			url: "/customer",
+			templateUrl: 'pages/users.html',
+			controller: 'ViewUsers'
+		})
 });
 
-app.filter('capitalize', function() {
-    return function(input) {
-        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
-    }
+app.filter('capitalize', function () {
+	return function (input) {
+		return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+	}
 });
 
 app.factory('Scopes', function ($rootScope) {
@@ -68,9 +68,9 @@ app.run(['$rootScope', function ($rootScope) {
 }]);
 
 //service
-app.factory('sessionService', function($rootScope,$http) {
+app.factory('sessionService', function ($rootScope, $http, $window) {
 	return {
-		refreshPage: function() {
+		refreshPage: function () {
 			$http.get('/sessioncheck').then(function (response) {
 				console.log("I got the data I requested");
 				console.log(response);
@@ -82,11 +82,10 @@ app.factory('sessionService', function($rootScope,$http) {
 					$window.location.href = "login.html";
 				} else {
 					$rootScope.username = response.data.username;
-					if(response.data.role =="tester") {
+					if (response.data.role == "tester") {
 						$rootScope.istester = true;
 						$rootScope.ismanager = false;
-					}
-					else{
+					} else {
 						$rootScope.istester = false;
 						$rootScope.ismanager = true;
 					}
@@ -112,16 +111,16 @@ app.controller('AddProjectController', ['$scope', '$http', '$window', function (
 
 		$http.post('/addproject', $scope.project).then(function (response) {
 			console.log(response);
-            $scope.project.description="";
-            $scope.project.name="";
-            $scope.project.enddate="";
-            $scope.project.startdate="";
+			$scope.project.description = "";
+			$scope.project.name = "";
+			$scope.project.enddate = "";
+			$scope.project.startdate = "";
 		});
 	};
 }]);
 
 //View Projects
-app.controller('ViewProjectsController', ['$scope', '$http', '$window', '$location','sessionService', function ($scope, $http, $window, $location,sessionService) {
+app.controller('ViewProjectsController', ['$scope', '$http', '$window', '$location', 'sessionService', function ($scope, $http, $window, $location, sessionService) {
 	console.log("ViewProjectsController");
 
 	//insert project details
@@ -139,8 +138,10 @@ app.controller('ViewProjectsController', ['$scope', '$http', '$window', '$locati
 
 	$scope.deleteProject = function (id) {
 		console.log($scope.project);
-		var data = {"id" :id};
-		$http.post('/deleteproject',data, $scope.project).then(function (response) {
+		var data = {
+			"id": id
+		};
+		$http.post('/deleteproject', data, $scope.project).then(function (response) {
 			console.log(response);
 			viewprojectlist();
 		});
@@ -148,18 +149,20 @@ app.controller('ViewProjectsController', ['$scope', '$http', '$window', '$locati
 
 	$scope.editProject = function (id) {
 		$scope.projectId = id;
-	// $location.path("editproject");
-		$http.get('/viewproject?id='+id).then(function (response) {
+		// $location.path("editproject");
+		$http.get('/viewproject?id=' + id).then(function (response) {
 			console.log(response);
 			$scope.projectData = response.data[0];
 			//viewprojectlist();
 		});
-	 };
+	};
 
 	$scope.viewProject = function (id) {
 		console.log($scope.project);
-		var data = {"id" :id};
-		$http.get('/viewproject?id='+id).then(function (response) {
+		var data = {
+			"id": id
+		};
+		$http.get('/viewproject?id=' + id).then(function (response) {
 			console.log(response);
 			$scope.projectData = response.data[0];
 			//viewprojectlist();
@@ -175,8 +178,14 @@ app.controller('ViewProjectsController', ['$scope', '$http', '$window', '$locati
 		var startdate = $scope.projectData.startdate;
 		var enddate = $scope.projectData.enddate;
 		var description = $scope.projectData.description;
-		var data = {"id" :id, "name":name,"startdate":startdate,"enddate":enddate,"description":description};
-		$http.post('/updateproject',data, $scope.project).then(function (response) {
+		var data = {
+			"id": id,
+			"name": name,
+			"startdate": startdate,
+			"enddate": enddate,
+			"description": description
+		};
+		$http.post('/updateproject', data, $scope.project).then(function (response) {
 			console.log(response);
 			viewprojectlist();
 		});
@@ -185,87 +194,101 @@ app.controller('ViewProjectsController', ['$scope', '$http', '$window', '$locati
 	};
 }]);
 
-app.controller('myProjectController', ['$scope', '$http', '$window', '$location','sessionService', function ($scope, $http, $window, $location,sessionService) {
-    console.log("ViewProjectsController");
+app.controller('myProjectController', ['$scope', '$http', '$window', '$location', 'sessionService', function ($scope, $http, $window, $location, sessionService) {
+	console.log("ViewProjectsController");
 
-    //insert project details
-    viewprojectlist = function () {
-
-
-        var data = {}//{ "id" : "59166393c8e1566f07f062e4"}
-        $http.post('/viewprojectlistForUser',data, $scope.project).then(function (response) {
-            console.log(response);
-            $scope.projectlist = response.data.docs;
-        });
-
-    };
-    //session refresh
-    sessionService.refreshPage();
-
-    $scope.deleteProject = function (id) {
-        console.log($scope.project);
-        var data = {"id" :id};
-        $http.post('/deleteproject',data, $scope.project).then(function (response) {
-            console.log(response);
-            viewprojectlist();
-        });
-    };
-
-    $scope.editProject = function (id) {
-        $scope.projectId = id;
-        // $location.path("editproject");
-        $http.get('/viewproject?id='+id).then(function (response) {
-            console.log(response);
-            $scope.projectData = response.data[0];
-            //viewprojectlist();
-        });
-    };
-
-    $scope.viewProject = function (id) {
-        console.log($scope.project);
-        var data = {"id" :id};
-        $http.get('/viewproject?id='+id).then(function (response) {
-            console.log(response);
-            $scope.projectData = response.data[0];
-            //viewprojectlist();
-        });
-    };
-
-    viewprojectlist();
-
-    $scope.update = function () {
-
-        var name = $scope.projectData.name;
-        var id = $scope.projectData._id;
-        var startdate = $scope.projectData.startdate;
-        var enddate = $scope.projectData.enddate;
-        var description = $scope.projectData.description;
-        var data = {"id" :id, "name":name,"startdate":startdate,"enddate":enddate,"description":description};
-        $http.post('/updateproject',data, $scope.project).then(function (response) {
-            console.log(response);
-            viewprojectlist();
-        });
-        // $location.path("editproject");
-
-    };
+	//insert project details
+	viewprojectlist = function () {
 
 
-    $scope.broadcast = function(project){
-    	$scope.selectedProject = project;
+		var data = {} //{ "id" : "59166393c8e1566f07f062e4"}
+		$http.post('/viewprojectlistForUser', data, $scope.project).then(function (response) {
+			console.log(response);
+			$scope.projectlist = response.data.docs;
+		});
+
+	};
+	//session refresh
+	sessionService.refreshPage();
+
+	$scope.deleteProject = function (id) {
+		console.log($scope.project);
+		var data = {
+			"id": id
+		};
+		$http.post('/deleteproject', data, $scope.project).then(function (response) {
+			console.log(response);
+			viewprojectlist();
+		});
 	};
 
-    $scope.BroadcastMessage = function(){
-        var data = {"project" :$scope.selectedProject , "tag":$scope.tag,"message":$scope.message};
-        $http.post('/broadcastMessage',data, $scope.project).then(function (response) {
-            console.log(response);
-            viewprojectlist();
-        });
+	$scope.editProject = function (id) {
+		$scope.projectId = id;
+		// $location.path("editproject");
+		$http.get('/viewproject?id=' + id).then(function (response) {
+			console.log(response);
+			$scope.projectData = response.data[0];
+			//viewprojectlist();
+		});
+	};
+
+	$scope.viewProject = function (id) {
+		console.log($scope.project);
+		var data = {
+			"id": id
+		};
+		$http.get('/viewproject?id=' + id).then(function (response) {
+			console.log(response);
+			$scope.projectData = response.data[0];
+			//viewprojectlist();
+		});
+	};
+
+	viewprojectlist();
+
+	$scope.update = function () {
+
+		var name = $scope.projectData.name;
+		var id = $scope.projectData._id;
+		var startdate = $scope.projectData.startdate;
+		var enddate = $scope.projectData.enddate;
+		var description = $scope.projectData.description;
+		var data = {
+			"id": id,
+			"name": name,
+			"startdate": startdate,
+			"enddate": enddate,
+			"description": description
+		};
+		$http.post('/updateproject', data, $scope.project).then(function (response) {
+			console.log(response);
+			viewprojectlist();
+		});
+		// $location.path("editproject");
+
+	};
+
+
+	$scope.broadcast = function (project) {
+		$scope.selectedProject = project;
+	};
+
+	$scope.BroadcastMessage = function () {
+		var data = {
+			"project": $scope.selectedProject,
+			"tag": $scope.tag,
+			"message": $scope.message
+		};
+		$http.post('/broadcastMessage', data, $scope.project).then(function (response) {
+			console.log(response);
+			viewprojectlist();
+		});
 
 	}
 }]);
 
 
-app.controller('EditProjectController', ['$rootScope','$scope', '$http', function ($rootScope, $scope, $http) {
+app.controller('EditProjectController', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
 	console.log("EditProjectController");
 	console.log($rootScope.projectId);
 	console.log($scope.projectId);
@@ -273,7 +296,7 @@ app.controller('EditProjectController', ['$rootScope','$scope', '$http', functio
 
 }]);
 
-app.controller('ProfileController', ['$scope', '$http','$window','sessionService', function ( $scope, $http, $window,sessionService) {
+app.controller('ProfileController', ['$scope', '$http', '$window', 'sessionService', function ($scope, $http, $window, sessionService) {
 	console.log("ProfileController");
 
 	//session refresh
@@ -303,9 +326,22 @@ app.controller('ProfileController', ['$scope', '$http','$window','sessionService
 		var portfolio = $scope.user.portfolio;
 		var hours = $scope.user.hours;
 		var available = $scope.user.available;
-		var data = {"name":name,"role":role,"last_name":last_name,"skills":skills,"organization":organization,"projects":projects,
-		"exp":exp,"sex":sex,"linkedin":linkedin,"portfolio":portfolio,"country":country,"hours":hours,"available":available};
-		$http.post('/updateprofile',data, $scope.project).then(function (response) {
+		var data = {
+			"name": name,
+			"role": role,
+			"last_name": last_name,
+			"skills": skills,
+			"organization": organization,
+			"projects": projects,
+			"exp": exp,
+			"sex": sex,
+			"linkedin": linkedin,
+			"portfolio": portfolio,
+			"country": country,
+			"hours": hours,
+			"available": available
+		};
+		$http.post('/updateprofile', data, $scope.project).then(function (response) {
 			console.log(response);
 			$window.alert("Profile Updated.");
 			viewprojectlist();
@@ -317,7 +353,7 @@ app.controller('ProfileController', ['$scope', '$http','$window','sessionService
 }]);
 
 //viewUser and profile
-app.controller('ViewUsers', ['$scope', '$http', '$window', '$location','sessionService', function ($scope, $http, $window, $location,sessionService) {
+app.controller('ViewUsers', ['$scope', '$http', '$window', '$location', 'sessionService', function ($scope, $http, $window, $location, sessionService) {
 	console.log("ViewUsers");
 
 	//session refresh
@@ -326,7 +362,7 @@ app.controller('ViewUsers', ['$scope', '$http', '$window', '$location','sessionS
 	viewusers = function () {
 		//console.log("Location --> " + $location.path);
 		$scope.user_role = $location.$$path.substring(1);
-		$http.get('/viewusers?role='+ $location.$$path.substring(1)).then(function (response) {
+		$http.get('/viewusers?role=' + $location.$$path.substring(1)).then(function (response) {
 			console.log(response);
 			$scope.users = response.data.docs;
 
@@ -338,56 +374,60 @@ app.controller('ViewUsers', ['$scope', '$http', '$window', '$location','sessionS
 
 	$scope.viewProfile = function (id) {
 		console.log($scope.project);
-		var data = {"id" :id};
-		$http.get('/viewprofile?id='+id).then(function (response) {
+		var data = {
+			"id": id
+		};
+		$http.get('/viewprofile?id=' + id).then(function (response) {
 			console.log(response);
 			$scope.profileData = response.data[0];
 		});
 	};
 
 	$scope.viewAllProjectsList = function (id) {
-		var data = {"id" :id};
+		var data = {
+			"id": id
+		};
 		$http.get('/viewprojectlist').then(function (response) {
-			console.log("tempProjectList : "+response);
+			console.log("tempProjectList : " + response);
 			$scope.tempProjectList = response.data.docs;
 		});
 
-        $scope.viewProfile(id)
+		$scope.viewProfile(id)
 	};
 
-    $scope.AssignProjectToUser = function (project) {
+	$scope.AssignProjectToUser = function (project) {
 
-        console.log("Gaurang :"+ project);
-        console.log("Gaurang :"+ $scope.profileData)
-        var data = {"project" :project , "profileData": $scope.profileData };
+		console.log("Gaurang :" + project);
+		console.log("Gaurang :" + $scope.profileData)
+		var data = {
+			"project": project,
+			"profileData": $scope.profileData
+		};
 
 
-        $http({
-            method : "POST",
-            url : '/assignProject',
-            data : {
-                "project" :project ,
+		$http({
+			method: "POST",
+			url: '/assignProject',
+			data: {
+				"project": project,
 				"profileData": $scope.profileData
-            }
-        }).success(function(data) {
-            console.log("Response : "+response);
-            $scope.status = response.data;
+			}
+		}).success(function (data) {
+			console.log("Response : " + response);
+			$scope.status = response.data;
 
-        }).error(function(error) {
-            console.log("inside error");
-            console.log(error);
-            $scope.unexpected_error = false;
-            $scope.invalid_login = true;
-            $window.alert("unexpected_error");
-        });
+		}).error(function (error) {
+			console.log("inside error");
+			console.log(error);
+			$scope.unexpected_error = false;
+			$scope.invalid_login = true;
+			$window.alert("unexpected_error");
+		});
 
-    };
+	};
 
 
 
 
 
 }]);
-
-
-
